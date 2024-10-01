6138f89bc5241e690a5b3b25bc829b81322b813c
Levon <levon@ihatemalware.com>
2024-10-01 10:35:07 -0500

    updated data structure and json

--- scripts/script.js
+++ scripts/script.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
                     <div class="col-lg-${12/numJobs} mb-5"
                         <div class="card">
                             <div class="card-body bg-dark">
-                                <h5 class="card-title em-blue">${job.employer} - ${job.role}</h5>
+                                <h5 class="card-title"><span class="em-purple">${job.employer}</span> - <span class="em-blue">${job.role}</span></h5>
                                 <h6 class="card-subtitle mb-2 text-muted">${job.dates}</h6>
                                 <p class="card-text">${job.duties}</p>
                             </div>
